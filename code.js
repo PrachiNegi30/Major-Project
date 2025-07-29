223930dda933323b9b3fa984eef54d6bc6accd69
PrachiNegi30 <prachinegi3021@gmail.com>
2025-07-29 20:43:43 +0530

    second commit

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+console.log('hello')
 const password = document.getElementById('password');
 const cpassword = document.getElementById('cpassword');
 form.addEventListener('submit', (event) => {
